f4f1fd3adc46e260239f4e0ad911ae51d6eca240
phongdeeptryy88 <phong882k2@gmail.com>
2024-03-18 16:03:10 +0700

    login

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,31 +1,41 @@
 import {
-    Blog, ManageVoucher, Voucher, Home, Cart, ShipDetail, PaymentSuccess,
-    Order, ManagementBlog, AddProduct, ProductDetail,
-    Product
-} from '../pages/index';
-import {
-    LayoutAdmin,
-    LayoutDefault
-} from '../layouts/index';
-import {Admin} from '../pages';
+  Blog,
+  ManageVoucher,
+  Voucher,
+  Home,
+  Cart,
+  ShipDetail,
+  PaymentSuccess,
+  Order,
+  ManagementBlog,
+  AddProduct,
+  ProductDetail,
+  Product,
+} from "../pages/index";
+import { LayoutAdmin, LayoutDefault } from "../layouts/index";
+import { Admin } from "../pages";
+import Login from "../pages/Login/Login";
 
 const router = [
-    { path: '/', component: Home },
-    { path: '/blogpost', component: Blog },
-    { path: '/voucher', component: Voucher },
-    { path: '/cart', component: Cart },
-    { path: '/ship-detail', component: ShipDetail },
-    { path: '/payment-success', component: PaymentSuccess },
-    { path: '/admin/voucher', layout: LayoutAdmin, component: ManageVoucher },
-    { path: '/admin/blog', layout: LayoutAdmin, component: ManagementBlog },
-    { path: '/admin', component: Admin },
-    { path: '/product', component: Product },
-    { path: '/product/:id', component: ProductDetail },
-    { path: '/payment-success', layout: LayoutDefault, component: PaymentSuccess },
-    { path: '/admin/order', layout: LayoutAdmin, component: Order },
-    { path: '/admin/addproduct', component: AddProduct },
-
+  { path: "/", component: Home },
+  { path: "/blogpost", component: Blog },
+  { path: "/voucher", component: Voucher },
+  { path: "/cart", component: Cart },
+  { path: "/ship-detail", component: ShipDetail },
+  { path: "/payment-success", component: PaymentSuccess },
+  { path: "/admin/voucher", layout: LayoutAdmin, component: ManageVoucher },
+  { path: "/admin/blog", layout: LayoutAdmin, component: ManagementBlog },
+  { path: "/admin", component: Admin },
+  { path: "/product", component: Product },
+  { path: "/product/:id", component: ProductDetail },
+  {
+    path: "/payment-success",
+    layout: LayoutDefault,
+    component: PaymentSuccess,
+  },
+  { path: "/admin/order", layout: LayoutAdmin, component: Order },
+  { path: "/admin/addproduct", component: AddProduct },
+  { path: "/login", layout: LayoutDefault, component: Login },
 ];
 
-export { router }
-
+export { router };
---
+++ src/services/login.js
@@ -0,0 +1,22 @@
+import * as request from "../utils/request";
+const LOGIN = "auth/login";
+const GET_USER = "users/getuser";
+
+export const login = async (username, password) => {
+  const response = await request.post(LOGIN, {
+    username: username,
+    password: password,
+  });
+  return response; // Trả về đối tượng User từ backend
+};
+export const getUser = async (accesstoken) => {
+  console.log(accesstoken);
+  const userResponse = await request.get(GET_USER, {
+    headers: {
+      Authorization: `Bearer ${accesstoken}`, // Gửi token trong headers để xác thực yêu cầu
+    },
+  });
+
+  console.log(userResponse);
+  return userResponse.data; // Trả về đối tượng User từ backend
+};
